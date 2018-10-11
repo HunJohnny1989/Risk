c2ea311f2ff61df2206a5537ff3c6369e3425594
tomeeeS <sajtitamas1@hotmail.com>
2018-10-11 17:51:25 +0200

    added hasAgentOccupiedTheseContinents(MissionAgent agent, String... continentNames ) convenience funtion in Missions

--- Risk/src/model/dto/Missions.java
+++ Risk/src/model/dto/Missions.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Stream;
+import model.MissionAgent;
 import model.Player;
 
 /**
@@ -20,29 +22,35 @@ public class Missions {
         // adding kill-a-color MissionCards for each player in the game
         for( Player p: players ) {
             missions.add( new MissionCard( String.format( "Kill %s ( %s )", p.getName(), p.getColor() ),
-                                           ( (agent) -> agent.hasKilledPlayer( p.getColor() ) ) ) ); 
+                                           ( agent -> agent.hasKilledPlayer( p.getColor() ) ) ) ); 
         }
         
         
-          missions.add( new MissionCard( String.format("Conquer Asia and South America"), 
-                                            ( (agent) -> agent.hasOccupiedContinent( new Continent( "Asia" ))
-                                                      && agent.hasOccupiedContinent( new Continent( "South America" ) ))));
-          /*missions.add(new MissionCard( String.format("Conquer Asia and Africa"),
-                                            ( (agent) -> agent.hasOccupiedContinent()));                                           
-          missions.add(new MissionCard( String.format("Conquer North America and Africa"),
-                                            ( (agent) -> agent.hasOccupiedContinent()));   /*                                    
+          missions.add(new MissionCard( String.format("Conquer Asia and South America"), 
+                       agent -> hasAgentOccupiedTheseContinents( agent, "Asia", "South America") ));
+//          missions.add(new MissionCard( String.format("Conquer Asia and Africa"),
+//                                            ( agent -> agent.hasOccupiedContinent()));                                           
+//          missions.add(new MissionCard( String.format("Conquer North America and Africa"),
+//                                            ( agent -> agent.hasOccupiedContinent()));   /*                                    
           missions.add(new MissionCard( String.format("Conquer North America and Australasia"),
-                                            ( (agent) -> agent.hasOccupiedContinent()));   
-          missions.add(new MissionCard( String.format("Conquer Europe and South America and a 3rd continent of your choice"),
-                                            ( (agent) -> agent.hasOccupiedContinent()));   
-          missions.add(new MissionCard( String.format("Conquer Europe and Australasia and a 3rd continent of your choice"),
-                                            ( (agent) -> agent.hasOccupiedContinent()));   
-          missions.add(new MissionCard( String.format("Occupy 18 territories with at least 2 armies in each territory"),
-                                            ( (agent) -> agent.hasOccupiedContinent()));   
-       
-          missions.add("Occupy 24 territories (no restriction to 2 or more armies in each)"); 
-          */
+                       ( agent -> hasAgentOccupiedTheseContinents( agent, "North America", "Australia", "Asia") )));   
+//          missions.add(new MissionCard( String.format("Conquer Europe and South America and a 3rd continent of your choice"),
+//                                            ( agent -> agent.hasOccupiedContinent()));   
+//          missions.add(new MissionCard( String.format("Conquer Europe and Australasia and a 3rd continent of your choice"),
+//                                            ( agent -> agent.hasOccupiedContinent()));   
+//          missions.add(new MissionCard( String.format("Occupy 18 territories with at least 2 armies in each territory"),
+//                                            ( agent -> agent.hasOccupiedContinent()));   
+//       
+//          missions.add("Occupy 24 territories (no restriction to 2 or more armies in each)"); 
+          
     }
+
+    private static boolean hasAgentOccupiedTheseContinents(MissionAgent agent, String... continentNames ) {
+        return Stream.of( continentNames ).collect( () -> false,
+                (b, s) -> agent.hasOccupiedContinent( new Continent( s )),
+                Boolean::logicalOr) ;
+    }
+    
     
     public static List< MissionCard > getMissions(){
         return missions;
